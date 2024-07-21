15b7b75c8ebcc50c82c0f607bbd3e13360c19793
idobar1 <idobar1@mail.tau.ac.il>
2024-07-22 00:05:57 +0300

    added functions for modularity

--- open_audio.py
+++ open_audio.py
@@ -23,19 +23,22 @@ left_ch = left_ch[:(fs*60)]
 t = np.arange(0,60,1/fs)
 
 left_ch = left_ch/np.max(left_ch)
-# f, Pxx_left = ss.welch(left_ch,fs,nperseg=65536)
-# plt.plot(f, Pxx_left)
-# plt.show()
+
+def plot_est_spectrum(x,fs,nperseg=65536):
+    f, Pxx_left = ss.welch(left_ch,fs,nperseg=nperseg)
+    plt.semilogy(f,Pxx_left)
+    plt.grid()
+    # plt.plot(f, Pxx_left)
+    plt.show()
+    
 def onset_func(x):
     E = np.power(x,2)
     dE_pos = np.diff(E,append=0)
     dE_pos[dE_pos<0] = 0
     O_t = dE_pos-np.mean(dE_pos)
     return O_t
-# E_left = np.power(left_ch,2)
-# dE_left_plus = np.diff(E_left,append=0)
-# dE_left_plus[dE_left_plus<0] = 0
-# O_t = dE_left_plus
+
+plot_est_spectrum(left_ch,fs,4096)
 O_t = onset_func(left_ch)
 plt.plot(t,O_t)
 plt.show()
