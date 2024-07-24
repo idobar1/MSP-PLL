8bd5d19e72f372e2d244ea6d5ad895c194156b85
idobar1 <idobar1@mail.tau.ac.il>
2024-07-24 19:39:59 +0300

    added phase compartaor

--- math_utils.py
+++ math_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.signal as ss
 import matplotlib.pyplot as plt 
 
-loop_gain = 3 ## TODO: Edit to reasonable value when known
+loop_gain = 0.5 ## TODO: Edit to reasonable value when known
 
 def plot_est_spectrum(x,fs,nperseg=65536):
     f, Pxx_left = ss.welch(x,fs,nperseg=nperseg)
@@ -11,8 +11,8 @@ def plot_est_spectrum(x,fs,nperseg=65536):
     # plt.plot(f, Pxx_left)
     plt.show()
     
-def onset_func(x):
-    E = np.power(x,2)
+def onset_func(s_t):
+    E = np.power(s_t,2)
     dE_pos = np.diff(E,append=0)
     dE_pos[dE_pos<0] = 0
     O_t = dE_pos-np.mean(dE_pos) #remove "DC"
@@ -22,19 +22,23 @@ def onset_func(x):
  ##TODO: Debug
  ## Documnet the relation of T and the cutoff Frequency
  # Maybe add more complex "MA" Filter (with some decay)
-def loop_filter(theta_t,t,type,T=10):
+def loop_filter(theta,t,type,T=10):
     if(type == "MA"):
         if(t<T):
-            e_t = (1/(t+1))*np.sum(theta_t[0:t]) * loop_gain
+            e_t = (1/(t+1))*np.sum(theta[0:t]) * loop_gain
         else:
-            e_t = (1/T)*np.sum(theta_t[t-T:t]) * loop_gain
+            e_t = (1/T)*np.sum(theta[t-T:t]) * loop_gain
     elif(type == "gain"):
-        e_t = theta_t[t] * loop_gain
+        e_t = theta[t] * loop_gain
     else:
         print("Invalid filt_type")
         e_t = 0
     return e_t
     
+    
+def phase_comp(O_t,x_t):
+    theta_t = x_t*O_t
+    return theta_t
         
         
     
