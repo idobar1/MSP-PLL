786a2a33c771230fe720a7bb212a786db4ad5c61
Nadav Zilberman <nadavzil7@gmail.com>
2024-07-30 22:26:28 +0300

    Some order

--- math_utils.py
+++ math_utils.py
@@ -1,14 +1,15 @@
 import numpy as np
 import scipy.signal as ss
 import matplotlib.pyplot as plt 
-
-## TODO: Move parameters to a file with all of the program parameters (also related w/ filename etc..)
+from config import Config
 ## TODO: Edit parameters to reasonable value when known
-loop_gain       = 0.5
-loop_filt_type  = "MA"
-loop_filter_mem = 10
-VCO_gain        = 2*np.pi/100 
-f0              = 2 #[Hz]
+MathConfig = Config.MathConfig
+
+def normalize_signal(samples):
+    return samples/np.max(samples)
+
+def synth_square(t_vec,f,d_c=0.5):
+    return ss.square(2 * np.pi * f * t_vec, d_c)
 
 def plot_est_spectrum(x,fs,nperseg=65536):
     f, Pxx_left = ss.welch(x,fs,nperseg=nperseg)
@@ -18,24 +19,24 @@ def plot_est_spectrum(x,fs,nperseg=65536):
     plt.show()
     
 def onset_func(s_t):
-    E = np.power(s_t,2)
-    dE_pos = np.diff(E,append=0)
-    dE_pos[dE_pos<0] = 0
-    O_t = dE_pos-np.mean(dE_pos) #remove "DC"
+    E = np.power(s_t, 2)
+    dE_pos = np.diff(E, append=0)
+    dE_pos[dE_pos < 0] = 0
+    O_t = dE_pos - np.mean(dE_pos) #remove "DC"
     return O_t
 
 
  ##TODO: Debug
  ## Documnet the relation of T and the cutoff Frequency
  # Maybe add more complex "MA" Filter (with some decay)
-def loop_filter(theta,t,filt_type,T=10):
+def loop_filter(theta, t, filt_type, T=10):
     if(filt_type == "MA"):
-        if(t<T):
-            e_t = (1/(t+1))*np.sum(theta[0:t+1]) * loop_gain
+        if(t < T):
+            e_t = (1/(t + 1))*np.sum(theta[0:t+1]) * MathConfig.loop_gain
         else:
-            e_t = (1/T)*np.sum(theta[t-T:t]) * loop_gain
+            e_t = (1/T)*np.sum(theta[t-T:t]) * MathConfig.loop_gain
     elif(filt_type == "gain"):
-        e_t = theta[t] * loop_gain
+        e_t = theta[t] * MathConfig.loop_gain
     else:
         print("Invalid filt_type")
         e_t = 0
@@ -45,14 +46,15 @@ def phase_comp(O_t,x_t):
     theta_t = x_t*O_t
     return theta_t
 
-def VCO(e_t,x_phase_t,VCO_gain):
-    dphase = e_t*VCO_gain
+def VCO(e_t, x_phase_t, VCO_gain):
+    dphase = e_t * VCO_gain
     next_phase = x_phase_t + dphase
     return next_phase
 
-def PLL(Onsets,fs): ## TODO: Implement
+def PLL(Onsets, fs):
     x = np.zeros(Onsets.shape) 
     x_phase = np.zeros(Onsets.shape)
+    inst_f = np.zeros(Onsets.shape)
     theta = np.zeros(Onsets.shape) 
     e = np.zeros(Onsets.shape)
     #Random phase init
@@ -60,15 +62,28 @@ def PLL(Onsets,fs): ## TODO: Implement
     x[0] = np.sin(x_phase[0]) 
     
     #PLL Loop
-    for t in range(len(Onsets)-1):
-        theta[t] = phase_comp(Onsets[t],x[t])
-        e[t] = loop_filter(theta,t,loop_filt_type,T=loop_filter_mem)     
-        x_phase[t+1] = VCO(e[t],x_phase[t], VCO_gain)       
-        x[t+1] = np.sin(np.unwrap([2*np.pi*f0*t/fs+x_phase[t+1]]))
-        # x[t+1] = np.sin(np.unwrap([x_phase[t+1]]))
-        
-        if(t%10000==0): ## For debug
-            print(t/10000)
-    return x
+    for n in range(len(Onsets)-1):
+        theta[n] = phase_comp(Onsets[n],x[n])
+        e[n] = loop_filter(theta, n, MathConfig.loop_filt_type, T=MathConfig.loop_filter_mem)      
+        x_phase[n+1] = VCO(e[n],x_phase[n], MathConfig.VCO_gain) 
+        inst_f[n+1]  = MathConfig.f0 + (x_phase[n+1] - x_phase[n])*fs/(2 * np.pi)     
+        x[n+1] = np.sin(np.unwrap([2 * np.pi * MathConfig.f0 * n/fs + x_phase[n+1]])) 
 
+        if(n % 10000 == 0): ## For debug
+            print(f"{int(100*n/len(Onsets))}%")
+    print("100%")
+
+    return e, x
+
+def zero_cross_detect(x):
+    zc_vec = np.zeros(x.shape)
+    zc_vec[x > 0] = 1
+    zc_vec=np.diff(zc_vec)
+    zc_vec[zc_vec < 0]=0
+    return zc_vec
+
+def metronome_thresholding(x,th=0.999):
+    metronome = np.zeros(x.shape)
+    metronome[x>th]=1
+    return metronome
     
--- open_audio.py
+++ open_audio.py
@@ -7,48 +7,13 @@ import sounddevice as sd
 from scipy.io.wavfile import write as write_wav
 from pydub.playback import play
 
-fname = "Heart Of Glass.wav"
-format = 'wav'
-fname = "Daft Punk - Around the World.mp3" 
-format = 'mp3' 
+def open_audio_file(fname, format):
+    audio = AudioSegment.from_file(fname,format=format)
+    samples = np.array(audio.get_array_of_samples())
+    num_of_channels = audio.channels
+    fs = audio.frame_rate
 
-audio = AudioSegment.from_file(fname,format=format)
-samples = np.array(audio.get_array_of_samples())
-channels = audio.channels
-fs = audio.frame_rate
+    return audio, samples, num_of_channels, fs
 
-# seperate 2 channels
-left_ch = samples[::2]
-right_ch = samples[1::2]
-
-# taking few secs only
-# Could be done using pydub library also
-#TODO: move to parameters dedicated file
-cut_len_sec = 10
-cut_start_sec = 60
-cut_stop_sec = cut_start_sec+cut_len_sec
-left_ch = left_ch[(cut_start_sec*fs):(fs*cut_stop_sec)] 
-t = np.arange(0,cut_len_sec,1/fs)
-
-s_t = left_ch/np.max(left_ch) #normalize to avoid overflow in processing
-O_t = onset_func(s_t)
-x_t = PLL(O_t,fs)
-
-x_t_audio = AudioSegment(
-    ((x_t*32767).astype('int16')).tobytes(), 
-    frame_rate=fs,
-    sample_width=samples.dtype.itemsize, 
-    channels=1)
-x_t_audio.export("x_t.wav", format="wav")
-sd.play(x_t, fs)
-
-
-
-plt.figure(1)
-plt.title("Welch PSD Estimation")
-plot_est_spectrum(s_t,fs,4096)
-plt.figure(2)
-plt.plot(t,O_t)
-plt.title('Onset Function')
-plt.show()
-a=5
+def separate_channels(audio_samples):
+    return audio_samples[::2], audio_samples[1::2]
