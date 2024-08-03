a50958f4501279d2332c29da52f9b55278314c1c
Nadav Zilberman <nadavzil7@gmail.com>
2024-08-03 15:12:23 +0300

    Update config, sync metronome

--- main.py
+++ main.py
@@ -33,15 +33,21 @@ def main():
     s_t = normalize_signal(processing_samples)
     O_t = math_utils.onset_func(s_t)
     e_t, x_t = math_utils.PLL(O_t, fs)
+    metronome = metronome_thresholding(x_t)
+    synched_metronome = math_utils.sync_metronome(metronome, O_t, fs, 1)
 
-    m_t = metronome_thresholding(x_t)
-    x_t_save_2 = m_t + s_t
-
+    m_t = np.convolve(synched_metronome, np.ones((int(fs*0.1))), mode="same" )
+    m_t_mod = np.multiply(m_t, np.cos(2*np.pi*440*t) + 0.25*np.cos(2*np.pi*440*3/2*t) + 0.1*np.cos(2*np.pi*440*5/4*t))
+    
+    m_t_mod = 2*m_t_mod/np.max(np.abs(m_t_mod))
+    x_t_save_2 = m_t_mod + s_t
+    
     x_t_audio = AudioSegment(
     ((x_t_save_2*32767/max(abs(x_t_save_2))).astype('int16')).tobytes(),
     frame_rate=fs,
     sample_width=samples.dtype.itemsize, 
     channels=1) 
+    
 
     x_t_audio.export("song_w_metro.wav", format="wav")
     plt.figure(1)
@@ -53,6 +59,12 @@ def main():
     plt.show()
     plt.figure(3)
     # plt.plot(t, x_t_mod)
+    plt.plot(t, metronome)
+    plt.title("metronome")
+    plt.show()
+    plt.plot(t, synched_metronome)
+    plt.title("synched_metronome")
+    plt.show()
     plt.plot(t, x_t)
     plt.title("x_t - metronome")
     plt.show()
--- math_utils.py
+++ math_utils.py
@@ -1,9 +1,8 @@
 import numpy as np
 import scipy.signal as ss
 import matplotlib.pyplot as plt 
-# from config import Config
-## TODO: Edit parameters to reasonable value when known
-# MathConfig = Config.MathConfig
+from config import Config
+MathConfig = Config.MathConfig
 
 def normalize_signal(samples):
     return samples/np.max(samples)
@@ -51,26 +50,26 @@ def VCO(e_t, x_phase_t, VCO_gain):
     next_phase = x_phase_t + dphase
     return next_phase
 
-def PLL(Onsets, fs):
-    x = np.zeros(Onsets.shape) 
-    x_phase = np.zeros(Onsets.shape)
-    inst_f = np.zeros(Onsets.shape)
-    theta = np.zeros(Onsets.shape) 
-    e = np.zeros(Onsets.shape)
+def PLL(pll_input, fs):
+    x = np.zeros(pll_input.shape) 
+    x_phase = np.zeros(pll_input.shape)
+    inst_f = np.zeros(pll_input.shape)
+    theta = np.zeros(pll_input.shape) 
+    e = np.zeros(pll_input.shape)
     #Random phase init
     x_phase[0] = 2*np.pi*np.random.rand()
     x[0] = np.sin(x_phase[0]) 
     
     #PLL Loop
-    for n in range(len(Onsets)-1):
-        theta[n] = phase_comp(Onsets[n],x[n])
+    for n in range(len(pll_input)-1):
+        theta[n] = phase_comp(pll_input[n],x[n])
         e[n] = loop_filter(theta, n, MathConfig.loop_filt_type, T=MathConfig.loop_filter_mem)      
         x_phase[n+1] = VCO(e[n],x_phase[n], MathConfig.VCO_gain) 
         inst_f[n+1]  = MathConfig.f0 + (x_phase[n+1] - x_phase[n])*fs/(2 * np.pi)     
         x[n+1] = np.sin(np.unwrap([2 * np.pi * MathConfig.f0 * n/fs + x_phase[n+1]])) 
 
         if(n % 10000 == 0): ## For debug
-            print(f"{int(100*n/len(Onsets))}%")
+            print(f"{int(100*n/len(pll_input))}%")
     print("100%")
 
     return e, x
@@ -82,18 +81,21 @@ def zero_cross_detect(x):
     zc_vec[zc_vec < 0]=0
     return zc_vec
 
-def metronome_thresholding(x,th=0.999):
+def metronome_thresholding(x, th=0.999):
     metronome = np.zeros(x.shape)
     metronome[x>th]=1
     return metronome
     
-def find_metronome_delay(onsets,metronome):
+def find_metronome_delay(metronome, onsets):
     max_ix_onsets = np.argmax(onsets)
     possible_delays = (np.where(metronome>0)[0])-max_ix_onsets
     delay_ix = np.argmin(np.abs(possible_delays))
     delay = possible_delays[delay_ix]
 
     return delay
-## TODO: use np.roll(metronome,delay)
 
-
+def sync_metronome(metronome, onset, fs, num_of_sec_trim):
+    start_val = int(fs*num_of_sec_trim)
+    metronome_delay = find_metronome_delay(metronome[-start_val:], onset[-start_val:])
+    metronome_synched = np.roll(metronome, -metronome_delay)
+    return metronome_synched
